08d17c5301a2649ba8c57b9a489129614bf76944
Alison Peterson (SDSU) <150858246+apeterson5-sdsu@users.noreply.github.com>
2025-09-09 09:08:57 -0700

    Add files via upload
    Fix: Added -y flags to apt and --force to ufw to avoid interactive prompts

--- scripts/ciscat-configs.sh
+++ scripts/ciscat-configs.sh
@@ -3,11 +3,11 @@
 # cis-cat configure additional settings.
 
 #2.2.4 Ensure telnet client is not installed
-sudo apt purge telnet
+sudo apt purge -y telnet
 
 # 3.5.1.3 Ensure ufw service is enabled
 sudo ufw allow proto tcp from any to any port 22
-sudo ufw enable
+sudo ufw --force enable
 
 #3.5.1.4 Ensure ufw loopback traffic is configured
 sudo ufw allow in on lo
@@ -27,7 +27,7 @@ sudo chown root:root /etc/ssh/sshd_config
 sudo chmod og-rwx /etc/ssh/sshd_config
 
 #5.4.1 Ensure password creation requirements are configured
-sudo apt install libpam-pwquality
+sudo apt install -y libpam-pwquality
 
 #5.5.4 Ensure default user umask is 027 or more restrictive
 sudo grep -RPi '(^|^[^#]*)\s*umask\s+([0-7][0-7][01][0-7]\b|[0-7][0-7][0-7][0-6]\b|[0-7][01][0-7]\b|[0-7][0-7][0-6]\b|(u=[rwx]{0,3},)?(g=[rwx]{0,3},)?o=[rwx]+\b|(u=[rwx]{1,3},)?g=[^rx]{1,3}(,o=[rwx]{0,3})?\b)' /etc/login.defs /etc/profile* /etc/bash.bashrc*
--- scripts/initial_configuration.sh
+++ scripts/initial_configuration.sh
@@ -2,20 +2,20 @@
 
 # Enable Firewall:
 sudo ufw allow ssh
-sudo ufw enable
+sudo ufw --force enable
 
 # Install Fail2Ban
-sudo apt install fail2ban
+sudo apt install -y fail2ban
 
 # Install Net-Tools for legacy network commands:
-sudo apt install net-tools
+sudo apt install -y net-tools
 
 # Install openssh-server:
-sudo apt install openssh-server
+sudo apt install -y openssh-server
 
 # Run updates:
 sudo apt update
-sudo apt upgrade
+sudo apt upgrade -y
 
 # Sleep must die:
 sudo systemctl mask sleep.target suspend.target hibernate.target hybrid-sleep.target
